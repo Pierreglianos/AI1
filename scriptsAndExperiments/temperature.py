8ec76720dab6f14258c61433a8c4d990c6acd4e0
EladShtiegmann <blackstratcs69@gmail.com>
2017-11-30 19:52:07 +0200

    Plot temp. graph

--- scriptsAndExperiments/temperature.py
+++ scriptsAndExperiments/temperature.py
@@ -2,6 +2,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 X = np.array([400,900,390,1000,550])
-
+minX = min(X)
+T = np.linspace(0.01, 5, 100)
+# xSum = np.sum(np.power(X, -1 * np.power(T, -1)))
 # TODO : Write the code as explained in the instructions
-raise NotImplementedError
+plt.axis([0, 5, 0, 1])
+plt.title("Probability as a function of the temperature")
+plt.ylabel("P")
+plt.xlabel("T")
+plt.grid()
+
+for x in X:
+    Y = [((np.power(x/minX, -1/t))/(np.sum(np.power(X/minX, -1/t)))) for t in T]
+    plt.plot(Y)
+    plt.show(block=False)
+    plt.waitforbuttonpress()
+
+plt.legend(X)
+plt.waitforbuttonpress()
