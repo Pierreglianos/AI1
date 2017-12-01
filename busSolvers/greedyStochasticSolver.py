261fb40e1a2a70e3fe4b7dbbe4aff142d9842681
Pierreglianos <piereglianos@gmail.com>
2017-12-01 14:50:13 +0200

    Implement getSuccProb

--- busSolvers/greedyStochasticSolver.py
+++ busSolvers/greedyStochasticSolver.py
@@ -6,7 +6,7 @@ class GreedyStochasticSolver(GreedySolver):
     _INITIAL_TEMPERATURE = None
     _N = None
 
-    def __init__(self, roads, astar, scorer, initialTemperature, temperatureDecayFactor, topNumToConsider):
+    def __init__(self, roads, astar, scoarrer, initialTemperature, temperatureDecayFactor, topNumToConsider):
         super().__init__(roads, astar, scorer)
 
         self._INITIAL_TEMPERATURE = initialTemperature
@@ -17,12 +17,17 @@ class GreedyStochasticSolver(GreedySolver):
         # Get the scores
         X = np.array([self._scorer.compute(currState, target) for target in successors])
 
+
         # Initialize an all-zeros vector for the distribution
         P = np.zeros((len(successors),))
 
         # TODO: Fill the distribution in P as explained in the instructions.
         # TODO : No changes in the rest of the code are needed
-        raise NotImplementedError
+        idxList = self._getMinN(X)
+        alpha = min(X);
+        denom = np.sum(np.power(X[idxList]/alpha, -1/self.T))
+        for i in idxList:
+            P[i] = [np.power(X[i]/alpha, -1/self.T)/denom]
 
         # Update the temperature
         self.T *= self._TEMERATURE_DECAY_FACTOR
@@ -38,7 +43,19 @@ class GreedyStochasticSolver(GreedySolver):
         # You should look for a suitable function in numpy.random.
         nextIdx = None
 
+
         return successors[nextIdx]
+
+    # Find the N smallest values in the array, returns these indices in a list
+    def _getMinN(self, arr):
+        indexList = []
+        newArr = np.copy(arr)
+        listSize = min(self._N, len(newArr))
+        for i in range(listSize):
+            minIndex = np.argmin(newArr)
+            indexList.append(minIndex)
+            newArr[minIndex] = np.inf
+        return indexList
 
     # Override the base solve method to initialize the temperature
     def solve(self, initialState):
